6e06eceb95055fe174120a84f9023e819fe5fc38
tianling <1033860952@qq.com>
2024-03-28 07:57:16 +0800

    1

--- src/leetcode_1_1000/Leetcode_332.java
+++ src/leetcode_1_1000/Leetcode_332.java
@@ -15,7 +15,9 @@ public class Leetcode_332 {
     public static void main(String[] args) {
         Leetcode_332 leetcode332=new Leetcode_332();
         // String[][] array = {{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"}};
-        String[][] array = {{"EZE","AXA"},{"TIA","ANU"},{"ANU","JFK"},{"JFK","ANU"},{"ANU","EZE"},{"TIA","ANU"},{"AXA","TIA"},{"TIA","JFK"},{"ANU","TIA"},{"JFK","TIA"}};
+        // String[][] array = {{"EZE","AXA"},{"TIA","ANU"},{"ANU","JFK"},{"JFK","ANU"},{"ANU","EZE"},{"TIA","ANU"},{"AXA","TIA"},{"TIA","JFK"},{"ANU","TIA"},{"JFK","TIA"}};
+        // String[][] array = {{"JFK","SFO"},{"JFK","ATL"},{"SFO","ATL"},{"ATL","JFK"},{"ATL","SFO"}};
+        String[][] array = {{"JFK","SFO"},{"JFK","ATL"},{"SFO","JFK"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"},{"ATL","AAA"},{"AAA","BBB"},{"BBB","ATL"}};
         leetcode332.findItinerary(new LinkedList<>(StringUtil.convertToNestedList(array)));
 
     }
@@ -25,47 +27,90 @@ public class Leetcode_332 {
 
     private List<String> resultList;
 
-    private boolean[] isUsed;
+    private Map<String,List<String>> map;
+    private int total;
 
     public List<String> findItinerary(List<List<String>> tickets) {
-        tickets.sort(Comparator.comparing(a -> a.get(1)));
+        // tickets.sort(Comparator.comparing(a -> a.get(1)));
         pathList=new LinkedList<>();
         resultList=new LinkedList<>();
-        isUsed=new boolean[tickets.size()];
+        map=new HashMap<>();
+        total=tickets.size()+1;
 
+        for (List<String> ticket : tickets) {
+            add(ticket.get(0),ticket.get(1));
+        }
+        pathList.add("JFK");
 
+        backTracking("JFK");
         return resultList;
     }
 
 
-    private boolean backTracking(List<List<String>> tickets,String startLocation){
-        if (pathList.size()==tickets.size()+1){
+
+
+
+
+    private boolean backTracking(String startLocation){
+        if (pathList.size()==total){
             resultList=new LinkedList<>(pathList);
             return true;
         }
+        List<String> ticketList = map.get(startLocation);
+        if (ticketList==null || ticketList.isEmpty()){
+            return false;
+        }
+        int size = ticketList.size();
 
+        // 这里面会有个问题，比如有非常多的重复机票，我们回溯的时候可能重复机票递归的情况，这样会超时的
+        // 所以我们在同一层的时候，用一个set来存储下，如果这个机票之前已经有人走过了，且无法走通的情况下，则后面的机票就不用再重复了
 
-        for (int i = 0; i < tickets.size(); i++) {
-            // 如果已经被使用了，则直接返回false
-            if (isUsed[i]){
+        Set<String> repeatSet=new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            String ticketEnd = ticketList.get(i);
+            // 避免重复机票
+            if (repeatSet.contains(ticketEnd)){
                 continue;
             }
-            List<String> ticket = tickets.get(i);
-            if (ticket.get(0).equals(startLocation)) {
-                pathList.add(ticket.get(1));
-                isUsed[i]=true;
-                if (backTracking(tickets,ticket.get(1))) {
-                    return true;
-                }
+            repeatSet.add(ticketEnd);
 
-                pathList.remove(pathList.size()-1);
-                isUsed[i]=false;
+            pathList.add(ticketEnd);
+            ticketList.remove(i);
+            if (backTracking(ticketEnd)) {
+                return true;
             }
+            pathList.remove(pathList.size()-1);
+            ticketList.add(i,ticketEnd);
         }
 
         return false;
 
     }
+
+
+    private void add(String key,String value){
+        List<String> valueList = map.get(key);
+        if (valueList==null){
+            valueList=new LinkedList<>();
+            valueList.add(value);
+            map.put(key,valueList);
+        }else{
+            // valueList.add(value);
+            int size = valueList.size();
+            for (int i = 0; i < size; i++) {
+                if (valueList.get(i).compareTo(value)>0){
+                    valueList.add(i,value);
+                    return;
+                }
+            }
+            valueList.add(valueList.size(),value);
+
+        }
+
+
+    }
+
+
 
 
 
